9c912b5c28d7f2df757b2bde2d6a46b4a62f7eb0
Sakibur59 <sakiburrahman5978@gmail.com>
2026-05-09 21:36:09 +0600

    connet to destination collection of mongodb  to frontent add destination page

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const uri = process.env.MONGO_DB_URI;
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+    
+    const db = client.db('wanderlust');
+    const destinationsCollection = db.collection('destinations');
+
+    app.post('/destination', async (req, res) => {
+      const destinationData = req.body;
+      console.log('Received destination data:', destinationData);
+      const result = await destinationsCollection.insertOne(destinationData);
+      res.send(result);
+    });
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
